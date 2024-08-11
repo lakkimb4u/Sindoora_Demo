055b34b693b798192c667f848ef518a7388c6367
Ravi Shankar Lakkimsetty <lakkimb4u@gmail.com>
2024-08-11 19:23:12 +0530

    Initial Commit
    Initial Commit

---
+++ First_Console_App_Demo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("========== WELCOME TO GITHUB DEMO ============");
+Console.WriteLine("Press Enter to continue........"  );
+Console.ReadLine();
+
